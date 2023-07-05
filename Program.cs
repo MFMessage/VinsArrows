ef99a350b3c9357c1293667da31b309cd80d2d59
Dawson <Dawson@DESKTOP-OS3FEIE>
2023-07-05 00:05:29 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,121 @@
+﻿Arrow arrow = GetArrow();
+Console.WriteLine($" That arrow costs {arrow.GetCost()} gold.");
+
+Arrow GetArrow()
+{
+    ArrowHead arrowhead = GetHead();
+    Fletching fletching = GetFletch();
+    float shaft = GetShaft();
+
+    return new Arrow(arrowhead, shaft, fletching);
+}
+
+float AskForNumberInRange(string text, int min, int max)
+{
+    while (true)
+    {
+        Console.Write(text);
+        if (float.TryParse(Console.ReadLine(), out float result) && min <= result && max >= result)
+            return result;
+        else continue;
+    }
+}
+
+float GetShaft()
+{
+    float shaftLength = AskForNumberInRange("How long would you like the arrow shaft to be? (between 60 and 100 cm)", 60, 100);
+    return shaftLength;
+}
+
+Fletching GetFletch()
+{
+    while (true)
+    {
+        Console.Write("Choose a fletching feather type (Plastic, Turkey, or Goose)");
+        string fletchChoice = Console.ReadLine();
+
+        if (fletchChoice != null)
+            fletchChoice = fletchChoice.ToLower().Trim();
+
+        if (fletchChoice == "plastic")
+            return Fletching.Plastic;
+        else if (fletchChoice == "turkey")
+            return Fletching.Turkey;
+        else if (fletchChoice == "goose")
+            return Fletching.Goose;
+        else
+        {
+            Console.WriteLine("Invalid choice, try again.");
+            continue;
+        }
+    }
+}
+
+ArrowHead GetHead()
+{
+    while (true)
+    {
+        Console.Write("Choose an arrowhead type (Steel, Wood, or Obsidian)");
+        string headChoice = Console.ReadLine();
+
+        if (headChoice != null)
+            headChoice = headChoice.ToLower().Trim();
+
+        if (headChoice == "steel")
+            return ArrowHead.Steel;
+        else if (headChoice == "wood")
+            return ArrowHead.Wood;
+        else if (headChoice == "obsidian")
+            return ArrowHead.Obsidian;
+        else
+        {
+            Console.WriteLine("Invalid choice, try again.");
+            continue;
+        }
+    }
+}
+enum ArrowHead
+{
+    Steel,
+    Wood,
+    Obsidian
+}
+
+enum Fletching
+{
+    Plastic,
+    Turkey,
+    Goose
+}
+class Arrow
+{
+    public ArrowHead _arrowhead;
+    public float _shaft;
+    public Fletching _fletching;
+
+    public Arrow(ArrowHead arrowHead, float shaft, Fletching fletching)
+    {
+        _arrowhead = arrowHead;
+        _fletching = fletching;
+        _shaft = shaft;
+    }
+    public float GetCost()
+    {
+        float arrowheadCost = _arrowhead switch
+        {
+            ArrowHead.Steel => 10,
+            ArrowHead.Wood => 3,
+            ArrowHead.Obsidian => 5
+        };
+        float fletchingCost = _fletching switch
+        {
+            Fletching.Plastic => 10,
+            Fletching.Turkey => 5,
+            Fletching.Goose => 3
+        };
+        float shaftCost = 0.05f * _shaft;
+
+        return arrowheadCost + fletchingCost + shaftCost;
+    }
+}
+
