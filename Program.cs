e59a0bc1e4f0ac5946f14515884d814125e98304
MFMessage <careydawson34@gmail.com>
2023-07-08 23:04:41 -0500

    wtf am i doing...

--- Program.cs
+++ Program.cs
@@ -58,31 +58,17 @@ ArrowHead SetHead()
         Console.Write("Choose an arrowhead type (Steel, Wood, or Obsidian)");
         string headChoice = Console.ReadLine();
 
-        if (headChoice != null)
             headChoice = headChoice.ToLower().Trim();
-
-        if (headChoice == "steel")
-            return ArrowHead.Steel;
-        else if (headChoice == "wood")
-            return ArrowHead.Wood;
-        else if (headChoice == "obsidian")
-            return ArrowHead.Obsidian;
-        else
-        {
-            Console.WriteLine("Invalid choice, try again.");
-            continue;
-        }
+            return headChoice switch
+            {
+                "steel" => ArrowHead.Steel,
+                "wood" => ArrowHead.Wood, // WTF IS HAPPENING HERE HOW DO I MAKE IT SO IT WILL ASK AGAIN IF THE CHOICE IS INVALID???
+                "obsidian" => ArrowHead.Obsidian,
+                _ => ArrowHead.invalid
+            };
     }
 }
-enum ArrowHead
-{
-    Steel, Wood, Obsidian
-}
 
-enum Fletching
-{
-    Plastic, Turkey, Goose
-}
 class Arrow
 {
     private ArrowHead _arrowhead;
@@ -97,7 +83,11 @@ class Arrow
     }
 
     public ArrowHead GetHead() => _arrowhead;
-    public float GetShaft() => _shaft;
+    public float Shaft
+    {
+        get { return _shaft; }
+        set { _shaft = value = AskForNumberInRange("How long would you like the arrow shaft to be? (between 60 and 100 cm)", 60, 100); }
+    }
     public Fletching GetFletching() => _fletching;
 
     public float GetCost()
@@ -118,5 +108,23 @@ class Arrow
 
         return arrowheadCost + fletchingCost + shaftCost;
     }
+    float AskForNumberInRange(string text, int min, int max)
+    {
+        while (true)
+        {
+            Console.Write(text);
+            if (float.TryParse(Console.ReadLine(), out float result) && min <= result && max >= result)
+                return result;
+            else continue;
+        }
+    }
+}
+enum ArrowHead
+{
+    Steel, Wood, Obsidian, invalid
 }
 
+enum Fletching
+{
+    Plastic, Turkey, Goose, invalid
+}
